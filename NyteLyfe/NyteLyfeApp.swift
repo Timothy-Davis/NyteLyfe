e6c397db6baf063217ba128f6da0b7a7893df2a3
Timothy Davis <timothy@nytework.com>
2023-05-28 15:44:00 -0400

    Initial commit

---
+++ NyteLyfe/NyteLyfeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NyteLyfeApp.swift
+//  NyteLyfe
+//
+//  Created by Timothy Davis on 5/28/23.
+//
+
+import SwiftUI
+
+@main
+struct NyteLyfeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
